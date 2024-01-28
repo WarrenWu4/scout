cccc6f0a7a7c50f5cdb1d30bd5d937a1deb7db6e
minim0no <ahmed.idrees3110@gmail.com>
2024-01-28 06:30:31 -0600

    init speech

--- components/TCard.js
+++ components/TCard.js
@@ -1,42 +1,47 @@
-import { View, Text, StyleSheet, Pressable } from "react-native"
-import { Image } from "expo-image"
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Image } from "expo-image";
+import * as Speech from "expo-speech";
 
 export default function TCard() {
+    const speak = () => {
+        const thingToSay = "Hello World";
+        Speech.speak(thingToSay);
+    };
 
     return (
         <View style={styles.card}>
-            
             <View style={styles.cardControl}>
-
-                <Pressable style={styles.switchBtn} onPress={() => console.log("fuc u")} title="View">
-                    
+                <Pressable
+                    style={styles.switchBtn}
+                    onPress={() => speak}
+                    title="View"
+                >
                     <Image
                         style={{ width: 24, height: 24 }}
                         source={require("../assets/switch.svg")}
                         contentFit="contain"
                     />
                     <Text style={styles.switchBtnText}>English</Text>
-
                 </Pressable>
 
-                <Pressable onPress={() => console.log("fuc u")} title="Output">
-    
+                <Pressable onPress={() => speak} title="Output">
                     <Image
-                        style={{width: 32, height: 32}}
+                        style={{ width: 32, height: 32 }}
                         source={require("../assets/volumeOut.svg")}
                         contentFit="contain"
                     />
-
                 </Pressable>
-
             </View>
 
             <Text>
-            Lorem ipsum dolor sit amet consectetur. Aliquet arcu quis morbi ut at ipsum risus diam eu. Nunc nec at leo ultrices vel pellentesque amet. Risus sem eget blandit augue amet arcu convallis. Ut egestas consequat tincidunt morbi mi massa sagittis sed dolor.
+                Lorem ipsum dolor sit amet consectetur. Aliquet arcu quis morbi
+                ut at ipsum risus diam eu. Nunc nec at leo ultrices vel
+                pellentesque amet. Risus sem eget blandit augue amet arcu
+                convallis. Ut egestas consequat tincidunt morbi mi massa
+                sagittis sed dolor.
             </Text>
-
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -50,20 +55,28 @@ const styles = StyleSheet.create({
     },
     cardControl: {
         width: "100%",
-        display: "flex", flexDirection: "row",
-        justifyContent: "space-between", alignItems: "center",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
         marginBottom: 4,
     },
     switchBtn: {
-        paddingLeft: 12, paddingRight: 12,
-        paddingTop: 8, paddingBottom: 8,
-        display: "flex", flexDirection: "row",
-        justifyContent: "center", alignItems: "center",
-        columnGap: 8, backgroundColor: "#000",
+        paddingLeft: 12,
+        paddingRight: 12,
+        paddingTop: 8,
+        paddingBottom: 8,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        columnGap: 8,
+        backgroundColor: "#000",
         borderRadius: 4,
     },
     switchBtnText: {
-        fontSize: 16, fontWeight: "bold",
+        fontSize: 16,
+        fontWeight: "bold",
         color: "#F4C288",
-    }
-})
+    },
+});
